5d534d39b1ee8508f97057faab8359c0511f8599
olympia <oaxelou@inf.uth.gr>
2017-11-24 14:58:52 +0200

    bridge with ccr

--- G10.3/3.4/ccr_bridge.c
+++ G10.3/3.4/ccr_bridge.c
@@ -12,52 +12,7 @@
  * Since all of the cars have to act the same when they try to enter and exit bridge
  * the function for every car is the same.
  */
-#include "mtx_cond.h"
-
-
-#define CCR_DECLARE(label) pthread_mutex_t mtx##label, mtx_q##label; \
-                           pthread_cond_t queue##label;\
-                           volatile int no_q##label, count_loop##label;
-
-#define CCR_INIT(label) mtx_init(&mtx##label, __LINE__); \
-                        mtx_init(&mtx_q##label, __LINE__); \
-                        cond_init(&queue##label, __LINE__); \
-                        no_q##label = 0; count_loop##label = -1;
-
-#define CCR_EXEC(label, cond, body) mtx_lock(&mtx##label, __LINE__); \
-                                    mtx_lock(&mtx_q##label, __LINE__); \
-                                    while (!cond){ \
-                                      no_q##label++; \
-                                      if(count_loop##label >= 0){ \
-                                        count_loop##label++; \
-                                        \
-                                        if(count_loop##label == no_q##label){ \
-                                          count_loop##label = -1; \
-                                          mtx_unlock(&mtx##label, __LINE__); \
-                                        } \
-                                        else{ \
-                                          no_q##label--; cond_signal(&queue##label, __LINE__); \
-                                        } \
-                                      } \
-                                      else{ \
-                                        mtx_unlock(&mtx##label, __LINE__); \
-                                      }\
-                                      cond_wait(&queue##label, &mtx_q##label, __LINE__); \
-                                    } \
-                                    \
-                                    body \
-                                    \
-                                    if(no_q##label > 0){ \
-                                      count_loop##label = 0; \
-                                      no_q##label--; \
-                                      cond_signal(&queue##label, __LINE__); \
-                                    } \
-                                    else{ \
-                                      count_loop##label = -1; \
-                                      mtx_unlock(&mtx##label, __LINE__); \
-                                    } \
-                                    mtx_unlock(&mtx_q##label, __LINE__);
-// **************************************************
+#include "ccr.h"
 
 CCR_DECLARE(X);
 
