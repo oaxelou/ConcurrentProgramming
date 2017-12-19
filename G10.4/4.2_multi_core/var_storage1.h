7676f644812dcf105a96951a9c830b08f7c622f8
olympia <oaxelou@inf.uth.gr>
2017-12-19 16:22:17 +0200

    variables library (maybe done)

--- G10.4/4.2_multi_core/var_storage1.h
+++ G10.4/4.2_multi_core/var_storage1.h
@@ -42,17 +42,47 @@ typedef volatile struct global_variable{
   volatile struct global_variable *  next;
 }global_varT;
 
+/******************************* locals ***************************************/
 local_varT* init_local_list();
-global_varT* init_global_list();
 
 void destroy_local_list(local_varT *head, int print_flag);
-void destroy_global_list(global_varT *head, int print_flag);
+void abort_local_function(local_varT *head);
 
 local_varT* add_local_node(local_varT *head, local_varT *current, char *new_name, int new_value);
-global_varT* add_global_node(global_varT *head, global_varT *current, char *new_name, int new_value);
 
 void print_local_contents(local_varT *head);
+/*if 1: create it, if 0: require its existence*/
+local_varT *find_local_name(local_varT *head, char name[], int lvalue, int print_flag);
+
+/* name: e.g. "argv[3]", lvalue: if 1: create it, if 0: require its existence*/
+local_varT *find_local_array_name(local_varT *head, char name[], int lvalue, int print_flag);
+local_varT *create_local_array(local_varT *head, char array_name[], int new_last_index);
+local_varT *realloc_local_array(local_varT *head, local_varT *current, char array_name[], int old_last_index, int new_last_index);
+
+void modify_local_node(local_varT *head, char name[], int new_value, int print_flag);
+int read_local_node(local_varT *head, char name[], int print_flag);
+
+/****************************** globals ***************************************/
+global_varT* init_global_list();
+void destroy_global_list(global_varT *head, int print_flag);
+void abort_global_function(global_varT *head);
+
+global_varT* add_global_node(global_varT *head, global_varT *current, char *new_name, int new_value);
+
 void print_global_contents(global_varT *head);
+
+/*if 1: create it, if 0: require its existence*/
+global_varT *find_global_name(global_varT *head, char name[], int lvalue, int print_flag);
+
+/* name: e.g. "argv[3]", lvalue: if 1: create it, if 0: require its existence*/
+global_varT *find_global_array_name(global_varT *head, char name[], int lvalue, int print_flag);
+global_varT *find_global_head_prev(global_varT *head);
+global_varT *create_global_array(global_varT *head, char array_name[], int new_last_index);
+global_varT *realloc_global_array(global_varT *head, global_varT *current, char array_name[], int old_last_index, int new_last_index);
+
+void modify_global_node(global_varT *head, char name[], int new_value, int print_flag);
+int read_global_node(global_varT *head, char name[], int print_flag);
+
 /******************************************************************************/
 
 #endif
